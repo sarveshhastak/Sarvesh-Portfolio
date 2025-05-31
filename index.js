cd505766a1fe6653a2c6a16e580f83e4c35bb209
sarvesh hastak <sarveshhastak1309@gmail.com>
2025-05-31 16:58:53 +0530

    Updated Portfolio

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
+const path = require("path")
 require("dotenv").config()
 const app = express()
 app.use(express.json())
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+app.use(express.static("dist"));
+app.use(cors({ origin: "https://sarvesh-portfolio-o3yb.onrender.com", credentials: true }))
 app.use("/api/form", require("./routes/form.route"))
 app.use("*", (req, res) => {
-    res.status(404).json({ message: "Resource Not Found !" })
-})
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 mongoose.connect(process.env.MONGO_URL)
 mongoose.connection.once("open", () => {
     console.log("MongoDB Connected");
